ca5c9ae67ebb2c0687439b568e50d012b19f94a1
VainioM <miikkavainio1919@gmail.com>
2025-01-14 12:04:58 +0200

    Create Visited.jsx

---
+++ frontend/src/pages/Visited.jsx
@@ -0,0 +1,10 @@
+import '../css/Visited.css'
+
+function Visited(){
+    return <div className="favorites-empty">
+        <h2>You never visited outside of your home country.</h2>
+        <p>Did you go aboard? Which country did you visit?</p>
+    </div>
+}
+
+export default Visited
